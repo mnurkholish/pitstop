be346a9e90888df634840e77702cc8ca67268ab2
kholish <kholish.nur8306@gmail.com>
2026-07-07 18:40:40 +0700

    cookie middleware pas

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->encryptCookies(except: [
+            'pitstop_theme',
+            'pitstop_font_size',
+        ]);
+
         $middleware->alias([
             'role' => RoleMiddleware::class,
         ]);
